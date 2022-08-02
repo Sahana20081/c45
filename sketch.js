47818f1a7609bebeb15815d9190e16a326435cbf
Sahana20081 <94735338+Sahana20081@users.noreply.github.com>
2022-08-02 21:05:54 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,19 @@
+var background,backgroundImage;
+var ballon,ballonImage;
+var obstacleTop,obstacleTop1Image,obstacleTop2Image;
+var obstacleBottom,obsBottom1Img,obsBottom2Img,obsBottom3Img;
+var gameOver,gameOverImg;
+var restart,restartImg;
+var score = 0
+var gameState
+
+function preload() {
+  
+}
+function setup(){
+createCanvas(500,500);
+
+}
+function draw() {
+  background ("red");
+}
